787154d46151b7bf409c6af42d123355622c86df
Luca Giunti <giunti@apc.in2p3.fr>
2022-02-14 10:46:20 +0100

    Implemented wrapper for xspec models and its test

--- ogip_spectra/__init__.py
+++ ogip_spectra/__init__.py
@@ -1,7 +1,9 @@
 from .ogip_spectrum_dataset import *
 from .io_ogip import *
+from .models import *
 
 __all__ = [
     "StandardOGIPDataset",
     "StandardOGIPDatasetReader",
+    "XspecSpectralModel"
 ]
